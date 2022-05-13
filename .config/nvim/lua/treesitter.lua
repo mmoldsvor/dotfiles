d180212bd0364c0f4804a82074cc7ca47c80df18
Martin Moldsvor <martin@moldsvor.com>
2022-05-13 11:23:22 +0200

    Added cpp and python to treesitter, and increased the repeat delay for tmux

--- .config/nvim/lua/treesitter.lua
+++ .config/nvim/lua/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup {
-    ensure_installed = {"c"}, 
+    ensure_installed = {"c", "cpp", "python"}, 
     sync_install = false,
     ignore_install = {},
     highlight = {
